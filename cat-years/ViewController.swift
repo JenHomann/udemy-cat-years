841d2dd92e91df0bbc5b9651563ea4bffab7aa62
Jen Homann <jenhomann@Jens-MBP.igs2.com>
2017-11-15 14:57:44 -0600

    cat years app

--- cat-years/ViewController.swift
+++ cat-years/ViewController.swift
@@ -1,15 +1,22 @@
 //
 //  ViewController.swift
-//  cat-years
+//  Hello World
 //
-//  Created by Jen Homann on 11/15/17.
+//  Created by Jen Homann on 9/1/17.
 //  Copyright © 2017 Jen Homann. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var ageInput: UITextField!
+    @IBOutlet var ageLabel: UILabel!
+    
+    @IBAction func ageSubmitPressed(_ sender: Any) {
+        self.ageLabel.text = ("\(Int(ageInput.text!)! * 7)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
